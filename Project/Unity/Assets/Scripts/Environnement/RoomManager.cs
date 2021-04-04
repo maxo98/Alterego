dc07450dab1c7656f09b2ff94f7f8b88c07b8b1a
erwan-ash <60441163+erwan-ash@users.noreply.github.com>
2021-04-04 19:03:22 +0200

    Premier commit
    Generation de niveau aléatoire avec limite de salle OK !
    
    Seulement des salles de base pour un début, il faudra faire des design de salles plus tard

---
+++ Project/Unity/Assets/Scripts/Environnement/RoomManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public int NombreMaxRoom;
+    public int NombreActuelRoom = 0;
+    public bool stopGenerate = false;
+
+    private void Update()
+    {
+        if (NombreActuelRoom >= NombreMaxRoom)
+        {
+            stopGenerate = true;
+        }
+    }
+}
---
+++ Project/Unity/Assets/Scripts/Environnement/RoomSpawner.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public enum TypeEntree { Top, Bottom, Left, Right }
+    public TypeEntree roomNeeded;
+    // 1 --> need Bottom door
+    // 2 --> need Top door
+    // 3 --> need Left door
+    // 4 --> need Right door
+    private RoomManager rm;
+    public RoomTemplates templates;
+    public GameObject closedRoom;
+    public int rand;
+    public bool spawned = false;
+
+    Vector3 offset = new Vector3(0, 0, 0);
+    private void Start()
+    {
+        rm = FindObjectOfType<RoomManager>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    void Spawn()
+    {
+        if (spawned == false)
+        {
+            spawned = true;
+            if (!rm.stopGenerate)
+            {
+                switch (roomNeeded)
+                {
+                    case TypeEntree.Top: //Spawn une room avec un porte TOP
+                        offset.z += 1;
+                        rand = Random.Range(0, templates.topRooms.Length);
+                        Instantiate(templates.topRooms[rand], transform.position + offset, Quaternion.identity);
+                        break;
+                    case TypeEntree.Bottom: //Spawn une room avec un porte BOTTOM
+                        offset.z -= 1;
+                        rand = Random.Range(0, templates.bottomRooms.Length);
+                        Instantiate(templates.bottomRooms[rand], transform.position + offset, Quaternion.identity);
+                        break;
+                    case TypeEntree.Left: //Spawn une room avec un porte LEFT
+                        offset.x -= 1;
+                        rand = Random.Range(0, templates.leftRooms.Length);
+                        Instantiate(templates.leftRooms[rand], transform.position + offset, Quaternion.identity);
+                        break;
+                    case TypeEntree.Right: //Spawn une room avec un porte RIGHT
+                        offset.x += 1;
+                        rand = Random.Range(0, templates.rightRooms.Length);
+                        Instantiate(templates.rightRooms[rand], transform.position + offset, Quaternion.identity);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                switch (roomNeeded)
+                {
+                    case TypeEntree.Top: //Spawn une room avec un porte TOP
+                        offset.z += 1;
+                        rand = Random.Range(0, templates.topRooms.Length);
+                        Instantiate(templates.closedHaut, transform.position + offset, Quaternion.identity);
+                        break;
+                    case TypeEntree.Bottom: //Spawn une room avec un porte BOTTOM
+                        offset.z -= 1;
+                        rand = Random.Range(0, templates.bottomRooms.Length);
+                        Instantiate(templates.closedBas, transform.position + offset, Quaternion.identity);
+                        break;
+                    case TypeEntree.Left: //Spawn une room avec un porte LEFT
+                        offset.x -= 1;
+                        rand = Random.Range(0, templates.leftRooms.Length);
+                        Instantiate(templates.closedGauche, transform.position + offset, Quaternion.identity);
+                        break;
+                    case TypeEntree.Right: //Spawn une room avec un porte RIGHT
+                        offset.x += 1;
+                        rand = Random.Range(0, templates.rightRooms.Length);
+                        Instantiate(templates.closedDroite, transform.position + offset, Quaternion.identity);
+                        break;
+                    default:
+                        break;
+                }
+            }
+ 
+            
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EnvSpawnPoint"))
+        {
+            if (other.GetComponent<RoomSpawner>().spawned == false && spawned == false ) // Spawn un mur qui empêche d'aller dans le vide
+            {
+                Instantiate(closedRoom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;
+            
+        }
+    }
+
+}
